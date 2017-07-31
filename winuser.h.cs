e6e401007cadb9b6713a168e2c7a7809e0009fe0
pwonders <me@plainwonders.net>
2017-07-31 14:15:08 -0700

    added hit test msg and consts.

--- winuser.h.cs
+++ winuser.h.cs
@@ -316,5 +316,40 @@ namespace Microsoft.Win32
 		public const int SWP_NOSENDCHANGING = 0x0400;
 		public const int SWP_DEFERERASE = 0x2000;
 		public const int SWP_ASYNCWINDOWPOS = 0x4000;
+
+		[DllImport("user32.dll")]
+		public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+		public const int WM_NCHITTEST = 0x0084;
+
+		public const int HTERROR = (-2);
+		public const int HTTRANSPARENT = (-1);
+		public const int HTNOWHERE = 0;
+		public const int HTCLIENT = 1;
+		public const int HTCAPTION = 2;
+		public const int HTSYSMENU = 3;
+		public const int HTGROWBOX = 4;
+		public const int HTSIZE = HTGROWBOX;
+		public const int HTMENU = 5;
+		public const int HTHSCROLL = 6;
+		public const int HTVSCROLL = 7;
+		public const int HTMINBUTTON = 8;
+		public const int HTMAXBUTTON = 9;
+		public const int HTLEFT = 10;
+		public const int HTRIGHT = 11;
+		public const int HTTOP = 12;
+		public const int HTTOPLEFT = 13;
+		public const int HTTOPRIGHT = 14;
+		public const int HTBOTTOM = 15;
+		public const int HTBOTTOMLEFT = 16;
+		public const int HTBOTTOMRIGHT = 17;
+		public const int HTBORDER = 18;
+		public const int HTREDUCE = HTMINBUTTON;
+		public const int HTZOOM = HTMAXBUTTON;
+		public const int HTSIZEFIRST = HTLEFT;
+		public const int HTSIZELAST = HTBOTTOMRIGHT;
+		public const int HTOBJECT = 19;
+		public const int HTCLOSE = 20;
+		public const int HTHELP = 21;
 	}
 }
